155baf46eb321687900fcabaa51e87cb766953b2
Fernando Reis <fmreis@gmail.com>
2020-01-11 20:05:38 +0000

    Identificador de ficheiro

--- src/main/java/io/fmreis/ajaxfileuploadexample/controller/UploadController.java
+++ src/main/java/io/fmreis/ajaxfileuploadexample/controller/UploadController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class UploadController {
@@ -19,8 +21,9 @@ public class UploadController {
         List<JsonResponse> jsonResponses = new ArrayList<>();
 
         for(int i = 0; i<file.length; i++){
-            System.out.println(file);
+            System.out.println(file[i].getOriginalFilename() + " " + file[i].getSize());
             jsonResponses.add(new JsonResponse(file[i].getOriginalFilename(), "Success"));
+
         }
 
         return jsonResponses;
